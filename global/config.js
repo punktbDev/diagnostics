1081bb4c7b4c519aee319f52c7d27144888cda30
punktbDev <punktb.dev@gmail.com>
2024-11-13 19:19:32 +0100

    policy

--- global/config.js
+++ global/config.js
@@ -1,3 +1,3 @@
 const API_URL = "https://vds.punkt-b.pro/api/v1"
-const POLICY_URL = "https://punkt-b.pro/diagnostic-policy"
+const POLICY_URL = "https://diag.punkt-b.pro/policy.html"
 const PROFESSIONS_ALL_URL = "https://diag.punkt-b.pro/43-professions-all.html"
