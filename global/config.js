d1718e0a5783a3abd97784131707d2f3f5280e95
Пункт Б: Разработка <145120621+punktbDev@users.noreply.github.com>
2025-07-26 11:34:31 +0300

    Update config.js
    intdata.ru

--- global/config.js
+++ global/config.js
@@ -1,3 +1,3 @@
 const API_URL = "https://vds.punkt-b.pro/api/v1"
-const POLICY_URL = "https://diag.punkt-b.pro/policy.html"
-const PROFESSIONS_ALL_URL = "https://diag.punkt-b.pro/43-professions-all.html"
+const POLICY_URL = "https://diag.intdata.ru/policy.html"
+const PROFESSIONS_ALL_URL = "https://diag.intdata.ru/43-professions-all.html"
